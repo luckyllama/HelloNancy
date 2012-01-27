f5235e14921503fc6ffbb7261c7929be2224591a
frank hadder <luckyllama@gmail.com>
2012-01-27 14:56:51 -0600

    removing stuff

--- App_Code/HelloWorldModule.cs
+++ App_Code/HelloWorldModule.cs
@@ -8,28 +8,16 @@ using Simple.Data;
 /// Summary description for HelloWorldModule
 /// </summary>
 public class HelloWorldModule : NancyModule
-{
-    private static readonly string DbPath = System.IO.Path.Combine
-				( System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory)
-				, @"App_Data\HelloNancy.sdf");
-
-	
+{	
 	public HelloWorldModule()
 	{
 		Get["/"] = parameters => {
-			return "Hello, Nancy!";
+			return "Hello, Intermark!";
 		};
 		
 		Get["/test"] = parameters => {
 			return "This is the Test route";
 		};
-		
-		Get["/spark"] = parameters => {
-			var db = Database.OpenFile(DbPath);
-			Person model = db.Persons.FindById(1);
-			if (model == null)
-				model = new Person { FirstName = "Jane", LastName = "Doe" };
-			return View["spark", model];
-		};
+
 	}
 }
